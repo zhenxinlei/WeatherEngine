8365a150e2383722ae546b0a2eb14922f5eeaf32
Real <realma@Sabre-2.local>
2017-04-03 23:18:59 -0400

    init

--- dataFetcher.py
+++ dataFetcher.py
@@ -1,11 +1,11 @@
 from pprint import pprint
 import requests
 r = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-pprint(r.json())
+#pprint(r.json())
 
 json = r.json()
 
-print('##########################')
+print('########ma9##################')
 pprint(json['query']['results']['channel']['item']['condition']['temp'])
 
 
